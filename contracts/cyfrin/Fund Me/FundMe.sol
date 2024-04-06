db7272df89585c834267efa79c6cc1610b2c735d
Starrpix <itzarbin@gmail.com>
2024-04-06 21:03:54 +0545

    update

--- contracts/cyfrin/Fund Me/FundMe.sol
+++ contracts/cyfrin/Fund Me/FundMe.sol
@@ -1,11 +1,40 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
+
 contract FundMe {
-     uint256 public minimumUsd = 6;
+     uint256 public minimumUsd = 5e18;
+
+     address[] public funders;
+     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+
 function fund() public payable{
-require (msg.value > minimumUsd,"Less than 1 ETH"); //1 eth
+require (getConversionRate(msg.value) > minimumUsd,"Less than 1 ETH"); //1 eth
+funders.push(msg.sender);
+addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 }
 
+function getPrice() public view returns (uint256){
+//Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+//ABI 
+AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+(,int256 price,,,) = priceFeed.latestRoundData();
+return uint256(price * 1e10) ;
+}
+
+function getConversionRate(uint256 ethAmount) public view returns (uint256){
+     uint256 ethPrice = getPrice();
+     uint256 ethAmountInUsd = (ethPrice * ethAmount)/1e18;
+     return ethAmountInUsd;
+
+}
+
+function getVersion() public view returns (uint256){
+    return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+}
 
 }
--- contracts/cyfrin/Fund Me/PriceConverter.sol
+++ contracts/cyfrin/Fund Me/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from '@chainlink/contracts/src/v0.8/AutomationCompatibleInterface.sol';
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // Why is this a library and not abstract?
 // Why not an interface?
