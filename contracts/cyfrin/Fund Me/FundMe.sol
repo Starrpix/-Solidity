e95253e73978b97214d3753b73609162f09f0156
Starrpix <itzarbin@gmail.com>
2024-04-08 20:14:55 +0545

    Update FundMe.sol

--- contracts/cyfrin/Fund Me/FundMe.sol
+++ contracts/cyfrin/Fund Me/FundMe.sol
@@ -4,22 +4,33 @@ pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
 
+error notOwner();
+
 contract FundMe {
      using PriceConverter for uint256;
-     uint256 public minimumUsd = 5e18;
+     uint256 public constant MINIMUM_USD = 50 * 1e18;
 
      address[] public funders;
      mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+     address public immutable i_owner;
+
+
+
+     constructor() {
+     i_owner = msg.sender;
+     }
+
 
 function fund() public payable{
-require ((msg.value.getConversionRate()) > minimumUsd,"Less than 1 ETH"); //1 eth
+require ((msg.value.getConversionRate()) > MINIMUM_USD,"Less than 1 ETH"); //1 eth
 funders.push(msg.sender);
 addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
 }
 
-function withdraw() public{
 
+function withdraw() public onlyOwner{
+     
      for ( uint256 funderIndex = 0 ; funderIndex < funders.length; funderIndex++) {
           address funder = funders[funderIndex];
           addressToAmountFunded[funder] = 0;
@@ -42,5 +53,21 @@ function withdraw() public{
     require (callSuccess,"Call Failed");
 
 }
+
+modifier onlyOwner{
+    // require(msg.sender == i_owner,"Must be owner");
+     if(msg.sender != i_owner) {revert notOwner();}
+     _;
+}
+
+// What happens if someone sends this contract ETH without calling fund function
+
+ receive() external payable {
+     fund();
+  }
+
+  fallback() external payable { 
+     fund();
+  }
 
 }
